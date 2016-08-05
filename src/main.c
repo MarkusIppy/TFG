8fab99b337d57822637c42a5b35e0ee4cf7c1661
asecking <asecking@gmail.com>
2016-08-05 12:50:24 +0200

    Ciertas modificaciones en la estructura

--- src/main.c
+++ src/main.c
@@ -10,23 +10,27 @@
 #define OBDV_MAXSTR 32
 
 int main() {
-    int fd, i = 0, n, status, parameter = 5;
+    int fd, i, n, status, parameter = 17;
     char answer[MAX_ANSWER];
-    char name[MAX_ANSWER];
-    sprintf(name,"%ld.json", time(NULL));
-    FILE * fp;
-    fp = fopen(name, "w+");
-    OBD_value * values[10000]; //TODO change constant
+    char name[sizeof (long)];
+    OBD_value * values[MAX_ANSWER];
     OBD_vallist lista_val;
     OBD_value *value;
 
+    //Open a file with time stamp as name and giving it an extension
+    FILE * fp;
+    sprintf(name, "%ld.json", time(NULL));
+    fp = fopen(name, "w+");
     printf("Opening port..\n");
+
+    //Opening OBD port and checking if an error ocurred
     fd = openOBD_port();
     if (fd < 0) {
         fprintf(stderr, "Error while opening port\n");
         exit(1);
     }
 
+    //Syncing protocol
     printf("Syncing port..\n");
     n = sync_protocol(fd);
     while ((status = read_msg(fd, answer, MAX_ANSWER, -1)) > 0); //TODO no se la validez de esto
@@ -49,7 +53,8 @@ int main() {
     //            }
     //            values[i] = *value;
     //        }
-    if (n == OBD_OK) {
+
+    if (n == OBD_OK) { //Syncing protocol succeeded
         fprintf(fp, "{\"Samplelist\":[");
         for (i = 0; i <= 150; i++) {
             values[i] = obd_newvalue();
@@ -59,21 +64,24 @@ int main() {
                 //                obd_appendvalue(&lista_val, values[i]);
                 //                values[i]->obdv_ts = time(NULL);
                 //                values[i]->obdv_parameter = ENGINE_RPM;
-                timestamp(answer, fp, parameter, values[i]);
-                sleep(1);
-                if (n < 0) {
+                sleep(1); //TODO ver tiempo
+                if (n < 0) { //If something went wrong or finished reading, reset OBD and close the file
                     fprintf(fp, "{\"Pname\":\"EOF\",\"INTvalue\":-1,\"FLOATvalue\":-1.0,\"CHARvalue\":\"-1\",\"Ts\":%ld}]}", time(NULL));
                     write_atmsg(fd, "Z");
                     sleep(1);
                     close(fd);
                     return 0;
+                } else {
+                    timestamp(answer, fp, parameter, values[i]); //Printing on the file
                 }
             }
         }
         fprintf(fp, "{\"Pname\":\"EOF\",\"INTvalue\":-1,\"FLOATvalue\":-1.0,\"CHARvalue\":\"-1\",\"Ts\":%ld}]}", time(NULL));
     } else {
-        fprintf(stderr, "Error syncing protocol\n");
+        fprintf(stderr, "Error syncing protocol\n"); //Syncing protocol did not succeed
     }
+
+    //Reset OBD and close the file
     write_atmsg(fd, "Z");
     sleep(1);
     close(fd);
